59d2bc2853f66b05f8404d4642eae622155cbc31
UWFrankGu <UWFrankGu@users.noreply.github.com>
2018-11-04 20:42:15 -0600

    no dd in remote_trace python

--- ceph_remotetrace.py
+++ ceph_remotetrace.py
@@ -82,7 +82,6 @@ for i in range(0, machine_count):
 	invoke_remote_cmd(machines[i], command)
 
 for i in range(0, machine_count):
-	command = "sudo dd if=/dev/sdb2 of=/dev/sdb1 bs=1M;"
 	command =  "sudo cp -aR " + data_dirs[i] + " " + data_dir_snapshots[i] + ";"
 	command += "sudo rm -rf " + trace_files[i]
 	invoke_remote_cmd(machines[i], command)
